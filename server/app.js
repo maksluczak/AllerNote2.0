36613e8ef1958690e4b1e249b88b3cc96879fe33
maksluczak <maksluczak99@gmail.com>
2025-08-07 20:16:06 +0200

    minor app router changes

--- server/app.js
+++ server/app.js
@@ -9,7 +9,10 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }));
 
 app.use('/', routes);
 app.use('/auth', auth);
