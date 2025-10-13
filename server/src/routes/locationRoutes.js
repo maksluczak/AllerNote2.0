0b3a6d01c7d8286dcd5c4f191a3e84daade771a1
maksluczak <maksluczak99@gmail.com>
2025-10-13 19:35:43 +0200

    routes updated

--- server/src/routes/locationRoutes.js
+++ server/src/routes/locationRoutes.js
@@ -4,6 +4,6 @@ const locationController = require("../controllers/locationController");
 const router = express.Router();
 
 router.post("/", locationController.createLocation);
-router.get("/", locationController.getLocationByVoivodeshipName);
+router.get("/:voivodeship", locationController.getLocationByVoivodeshipName);
 
 module.exports = router;
