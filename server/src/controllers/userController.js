fee6880e711018ee7399bcaec92f1556018246a7
maksluczak <maksluczak99@gmail.com>
2025-10-14 22:43:42 +0200

    location router for user

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,59 +1,91 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const getUserById = async (req, res) => {
-    try {
-        if (!req?.params?.id) {
-            return res.status(400).json({ 'message': 'ID is required' });
-        }
-        const userId = req.params.id;
-        const user = await User.findById(userId).exec();
-        if (!user) {
-            return res.status(204).json({ 'message': 'no user matches id' });
-        }
-        return res.json(user);
-    } catch (err) {
-        return res.status(404).json({ error: err.message });
+  try {
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "ID is required" });
     }
+    const userId = req.params.id;
+    const user = await User.findById(userId).exec();
+    if (!user) {
+      return res.status(204).json({ message: "no user matches id" });
+    }
+    return res.json(user);
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
 };
 
 const updateUsernameByUserId = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const { username } = req.body;
+  try {
+    const userId = req.params.id;
+    const { username } = req.body;
 
-        if (!username) {
-            return res.status(400).json({ message: 'Username is required' });
-        }
-
-        const user = await User.findByIdAndUpdate(userId, { username }, { new: true} );
-        return res.status(201).json({ "message": `Username updated: ${user}`});
-    } catch (err) {
-        return res.status(404).json({ error: err.message });
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
     }
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { username },
+      { new: true }
+    );
+    return res.status(201).json({ message: `Username updated: ${user}` });
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
 };
 
 const updatePasswordByUserId = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const { password } = req.body;
+  try {
+    const userId = req.params.id;
+    const { password } = req.body;
 
-        if (!password) {
-            return res.status(400).json({ message: 'Password is required' });
-        }
-
-        const hashedPwd = await bcrypt.hash(password, 10);
-
-        const user = await User.findByIdAndUpdate(userId, { password: hashedPwd }, { new: true });
-        return res.status(201).json({ "message": `Password updated: ${user}`});
-    } catch (err) {
-        return res.status(404).json({ error: err.message });
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
     }
+
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { password: hashedPwd },
+      { new: true }
+    );
+    return res.status(201).json({ message: `Password updated: ${user}` });
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
 };
 
+const getUserLocation = async (req, res) => {
+  try {
+    const userId = req.user;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+
+    const defaultLocation = user.userDefaultLocation;
+
+    if (!defaultLocation) {
+      res.status(400).json({ message: "Location not found" });
+    }
+
+    return res.json(200).json({
+      defaultLocation: defaultLocation,
+      message: "Location found succesfully",
+    });
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
+};
 
 module.exports = {
-    getUserById,
-    updateUsernameByUserId,
-    updatePasswordByUserId
-}
+  getUserById,
+  updateUsernameByUserId,
+  updatePasswordByUserId,
+  getUserLocation,
+};
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const userController = require('../controllers/userController');
+const express = require("express");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/:id", userController.getUserById);
 router.put("/username/:id", userController.updateUsernameByUserId);
-router.put('/password/:id', userController.updatePasswordByUserId);
+router.put("/password/:id", userController.updatePasswordByUserId);
+router.get("/location", userController.getUserLocation);
 
 module.exports = router;
